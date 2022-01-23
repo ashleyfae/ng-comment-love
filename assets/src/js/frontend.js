160ebef71bea0730ba5bdb0a31557f954ffbdec1
Ashley Gibson <ashley@nosegraze.com>
2022-01-23 16:23:08 +0000

    Add docblocks.

--- assets/src/js/frontend.js
+++ assets/src/js/frontend.js
@@ -2,6 +2,9 @@
 
 import {apiRequest} from "./util/api";
 
+/**
+ * Set up initial listeners on DOM load.
+ */
 document.addEventListener( 'DOMContentLoaded', () => {
 	const siteUrl = getSiteUrl();
 
@@ -19,6 +22,11 @@ document.addEventListener( 'DOMContentLoaded', () => {
 	}
 } );
 
+/**
+ * Retrieves the entered site URL.
+ *
+ * @returns {string}
+ */
 function getSiteUrl() {
 	let siteUrl = NGLOVE.website_url;
 	const websiteElement = document.getElementById( 'url' );
@@ -29,6 +37,11 @@ function getSiteUrl() {
 	return siteUrl;
 }
 
+/**
+ * Fetches blog posts from the specified URL.
+ *
+ * @param {string} url
+ */
 function getPosts( url ) {
 	const messagesWrap = document.getElementById( 'comment-love-messages' );
 	const postsWrap = document.getElementById( 'comment-love-latest-posts' );
@@ -44,7 +57,6 @@ function getPosts( url ) {
 
 	apiRequest( '/posts', 'POST', {url} )
 		.then( response => {
-			console.log( 'Posts', response );
 			if ( ! response.posts.length ) {
 				throw new Error();
 			}
@@ -60,6 +72,12 @@ function getPosts( url ) {
 		} )
 }
 
+/**
+ * Builds the HTML list of blog posts.
+ *
+ * @param {object[]} posts
+ * @returns {string}
+ */
 function buildPostList( posts ) {
 	return `<ul>
 <li>
@@ -71,20 +89,24 @@ ${posts.map( buildPost ).join( "\n" )}
 }
 
 /**
+ * Builds the HTML for a single post.
  *
  * @param {object} post
  */
 function buildPost( post ) {
 	const selectedHtml = post.isSelected ? 'checked' : '';
 
-	return `
-<li>
+	return `<li>
 	<input type="radio" id="${post.identifier}" name="cl_post_title" value="${post.title}" data-url="${post.url}" ${selectedHtml}>
 	<label for="${post.identifier}">${post.title}</label>
 </li>
 `;
 }
 
+/**
+ * When the selected radio button changes, update the hidden field to
+ * set the selected URL.
+ */
 function addPostSelectListener() {
 	const postTitleInputs = document.getElementsByName( 'cl_post_title' );
 	if ( ! postTitleInputs ) {
@@ -104,6 +126,11 @@ function addPostSelectListener() {
 	} );
 }
 
+/**
+ * Saves the selected URL in a hidden field.
+ *
+ * @param {HTMLElement} radio
+ */
 function setSelectedUrl( radio ) {
 	const postUrlEl = document.getElementById( 'cl_post_url' );
 	if ( ! postUrlEl ) {
@@ -113,6 +140,11 @@ function setSelectedUrl( radio ) {
 	postUrlEl.value = radio.getAttribute( 'data-url' );
 }
 
+/**
+ * Adds an error message to the DOM.
+ *
+ * @param {string} message
+ */
 function addErrorMessage( message ) {
 	const messagesWrap = document.getElementById( 'comment-love-messages' );
 	if ( messagesWrap ) {
@@ -122,6 +154,12 @@ function addErrorMessage( message ) {
 	}
 }
 
+/**
+ * Determines if the supplied URL looks valid.
+ *
+ * @param {string} url
+ * @returns {boolean}
+ */
 function isValidUrl( url ) {
 	if ( typeof url == 'undefined' ) {
 		return false;
